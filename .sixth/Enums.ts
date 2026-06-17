b602d33a3dde4773f9c7b811ffeed08d990a73bc
feisaladen <feisaladen32@gmail.com>
2026-06-17 15:31:39 +0300

    today i learned couple of utilities used in typescript and Enums

---
+++ .sixth/Enums.ts
@@ -0,0 +1,17 @@
+  interface weeks {
+    monday : string ,
+    tuesday : string , 
+    wed : string , 
+    thursday : string ,
+  }
+  enum Weeks  {
+    monday = "monday",
+    tuesday = "tuesday",
+    wed = 'wed',
+    thursday = 'thursday'
+
+    let wkday = Weeks.monday 
+
+
+ 
+  }
